bffc3cb9b0aebdbdb52323e02d88381674b8367f
Matthew Leibowitz <mattleibow@live.com>
2023-12-07 14:40:25 +0200

    Add more code

---
+++ EmbeddedMauiApps.Embedding/EmbeddedWindowHandler.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Maui.Handlers;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+#if ANDROID
+using PlatformWindow = Android.App.Activity;
+using PlatformApplication = Android.App.Application;
+#elif IOS || MACCATALYST
+using PlatformWindow = UIKit.UIWindow;
+using PlatformApplication = UIKit.IUIApplicationDelegate;
+#elif WINDOWS
+#endif
+
+namespace Microsoft.Maui.Controls;
+
+internal class EmbeddedWindowHandler : ElementHandler<IWindow, PlatformWindow>, IWindowHandler
+{
+    public static IPropertyMapper<IWindow, IWindowHandler> Mapper =
+        new PropertyMapper<IWindow, IWindowHandler>(ElementHandler.ElementMapper)
+        {
+        };
+
+    public static CommandMapper<IWindow, IWindowHandler> CommandMapper =
+        new CommandMapper<IWindow, IWindowHandler>(ElementHandler.ElementCommandMapper)
+        {
+        };
+
+    public EmbeddedWindowHandler()
+        : base(Mapper)
+    {
+    }
+
+    protected override PlatformWindow CreatePlatformElement()
+    {
+#if ANDROID
+        var window = Microsoft.Maui.ApplicationModel.Platform.CurrentActivity!;
+#elif IOS || MACCATALYST
+        var window = Microsoft.Maui.ApplicationModel.Platform.GetCurrentUIViewController().View.Window;
+#elif WINDOWS
+// TODO
+#endif
+        return window;
+    }
+}
---
+++ EmbeddedMauiApps.Embedding/MauiAppBuilderExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection.Extensions;
+
+#if ANDROID
+using PlatformApplication = Android.App.Application;
+#elif IOS || MACCATALYST
+using PlatformWindow = UIKit.UIWindow;
+using PlatformApplication = UIKit.IUIApplicationDelegate;
+#elif WINDOWS
+#endif
+
+namespace Microsoft.Maui.Controls;
+
+public static class MauiAppBuilderExtensions
+{
+    public static MauiAppBuilder UseMauiEmbedding(this MauiAppBuilder builder, PlatformApplication platformApplication)
+    {
+        builder.Services.AddSingleton(platformApplication);
+
+        builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<IMauiInitializeService, EmbeddedInitializeService>());
+
+        builder.ConfigureMauiHandlers(handlers =>
+        {
+#if ANDROID || IOS || MACCATALYST
+            //handlers.AddHandler(typeof(Window), typeof(EmbeddedWindowHandler));
+#endif
+        });
+
+        return builder;
+    }
+
+    private class EmbeddedInitializeService : IMauiInitializeService
+    {
+        public void Initialize(IServiceProvider services) =>
+            EmbeddedPlatformApplication.Create(services);
+    }
+}
